15f7d927fb99ea5ded775bc6450690398764817e
komal-kumbhare-trellix <124895693+komal-kumbhare-trellix@users.noreply.github.com>
2024-08-22 15:55:34 +0530

    Adding refactored code and decorators

---
+++ alert_sync/event_parsing.py
@@ -0,0 +1,92 @@
+# import psycopg2
+import uuid
+
+
+class EventParsing:
+
+    # Function to generate a UUID
+    def generate_uuid(self):
+        return str(uuid.uuid4())
+
+    # Function to map severity to numerical value
+    def map_severity(self, severity):
+        if severity == "CRIT":
+            return 5
+        elif severity == "MAJR":
+            return 4
+        elif severity == "MINR":
+            return 3
+        else:
+            return 2
+
+    # Function to map product to sources
+    def map_sources(self, product):
+        if product == "WEB_MPS":
+            return "network"
+        elif product == "EMAIL_MPS":
+            return "email"
+        else:
+            return "unknown"
+
+    def parse_event_for_alert(self, json_data):
+        try:
+            # Loop through each alert in the JSON data
+            if isinstance(json_data, list):
+                for alert in json_data:
+                    alert_id = self.generate_uuid()
+                    name = alert["name"]
+                    severity = self.map_severity(alert["severity"])
+                    sources = self.map_sources(alert["product"])
+                    occurred = alert["occurred"]
+                    data = (
+                        alert_id,
+                        'c82ef4de-b80b-4610-81c6-261733d0d5c7',
+                        'hexint04sust01',
+                        name,
+                        '',  # message is kept empty
+                        severity,
+                        '2',  # confidence
+                        '0',  # risk
+                        alert["scVersion"],
+                        alert["product"],
+                        sources,
+                        '{*/T1102,T0000/T1103,T0001/T1103}',
+                        '{}',
+                        False,
+                        occurred
+                    )
+            elif isinstance(json_data, dict):
+                alert = json_data
+                alert_id = self.generate_uuid()
+                name = alert["name"]
+                severity = self.map_severity(alert["severity"])
+                sources = self.map_sources(alert["product"])
+                occurred = alert["occurred"]
+                data = (
+                    alert_id,
+                    'c82ef4de-b80b-4610-81c6-261733d0d5c7',
+                    'hexint04sust01-TEST',
+                    name,
+                    '',  # message is kept empty
+                    severity,
+                    '2',  # confidence
+                    '0',  # risk
+                    alert["sc-version"],
+                    alert["product"],
+                    sources,
+                    '{*/T1102,T0000/T1103,T0001/T1103}',
+                    '{}',
+                    False,
+                    occurred
+                )
+
+            else:
+                return 'Not dict or list'
+            return data
+        except Exception as error:
+            print(f"Error: {error}")
+
+
+    def parse_function(self, event):
+        #TO DO:logic to parse event
+        return event
---
+++ decorators/event_epoch.py
@@ -0,0 +1,70 @@
+# Event epoch class to convert timestamp
+import logging
+import datetime
+import pytz
+import json
+from functools import wraps
+
+# Define the EventEpoch class
+class EventEpoch:
+    def __init__(self):
+        pass
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(f"Executing {func.__name__} inside decorator")
+            alert = func(*args, **kwargs)
+            print(f"Decorating {func.__name__} with decorator {self.__class__.__name__}")
+            result = self.decorate(alert)
+            return result
+
+        return wrapper
+
+    def decorate(self, event):
+        date_time_priority_list = ['occurred','attack-time','eventtime', 'detectedtime', 'meta_ts', 'eventtimeutc']  # Replace with actual config
+
+        for date_time_str in date_time_priority_list:
+            if date_time_str in event:
+                try:
+                    if date_time_str == 'eventtimeutc':
+                        parsed_datetime = datetime.datetime.strptime(event[date_time_str], '%Y/%m/%d %H:%M:%S %Z%z')
+                    elif date_time_str == 'occurred' or date_time_str == 'attack-time':
+                        parsed_datetime = datetime.datetime.strptime(event[date_time_str], '%Y-%m-%dT%H:%M:%SZ')
+                    else:
+                        parsed_datetime = datetime.datetime.strptime(event[date_time_str], '%Y-%m-%dT%H:%M:%S.%fZ')
+                    self.add_event_epoch(event, parsed_datetime, date_time_str)
+                except ValueError:
+                    logging.debug(f"Failed to parse dateTime field {date_time_str}")
+        return event
+
+    def add_event_epoch(self, event, parsed_datetime, date_time_str):
+        zone = parsed_datetime.tzname()
+        event[date_time_str+'_epoch'] = {
+            "day": parsed_datetime.day,
+            "epochtime_field": date_time_str,
+            "hour": parsed_datetime.hour,
+            "minute": parsed_datetime.minute,
+            "month": parsed_datetime.month,
+            "seconds": parsed_datetime.second,
+            "timezone": zone.lower() if zone else 'utc',
+            "weekday": parsed_datetime.strftime('%A').lower(),
+            "year": parsed_datetime.year,
+        }
+
+# Usage
+if __name__ == '__main__':
+    event_epoch = EventEpoch()
+    print('Testing:::::::::')
+
+    # Read input JSON file
+    with open('decorators/events.json', 'r') as input_file:
+        events = json.load(input_file)
+
+    # Process events
+    for event in events:
+        event_epoch.decorate(event)
+
+    # Write output to JSON file
+    with open('decorators/event_epoch_test_output.json', 'w') as output_file:
+        json.dump(events, output_file, indent=4)
